ea187426064bcdc47e82a718a1748ece9cdab6ee
Denis Beresnev <shanthi.bunch@gmail.com>
2025-11-26 20:20:32 +0300

    refactor: updated config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,15 +1,15 @@
 package config
 
 type Config struct {
-	Env    string `mapstructure:"env"`
-	Addr   string `mapstructure:"addr"`
-	Logger Logger `mapstructure:"logger"`
+	Env    string `mapstructure:"env" yaml:"env"`
+	Addr   string `mapstructure:"addr" yaml:"addr"`
+	Logger Logger `mapstructure:"logger" yaml:"logger"`
 }
 
 type Logger struct {
-	App          string `mapstructure:"app"`
-	Level        string `mapstructure:"level"`
-	Service      string `mapstructure:"service"`
-	UDPAddress   string `mapstructure:"udp_address"`
-	EnableCaller bool   `mapstructure:"enable_caller"`
+	App          string `mapstructure:"app" yaml:"app"`
+	Level        string `mapstructure:"level" yaml:"level"`
+	Service      string `mapstructure:"service" yaml:"service"`
+	UDPAddress   string `mapstructure:"udp_address" yaml:"udp_address"`
+	EnableCaller bool   `mapstructure:"enable_caller" yaml:"enable_caller"`
 }
