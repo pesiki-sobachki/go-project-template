e924d0268079ab4460680320713f50c8c03d26a0
Denis Beresnev <shanthi.bunch@gmail.com>
2025-12-02 18:19:28 +0300

    feat: added memory store

---
+++ internal/pkg/store/memory.go
@@ -0,0 +1,47 @@
+package store
+
+import (
+	"context"
+	"sync"
+
+	"github.com/shanth1/gotools/errs"
+)
+
+var ErrNotFound = errs.ErrNotFound
+
+type MemoryStore[K comparable, V any] struct {
+	mu   sync.RWMutex
+	data map[K]V
+}
+
+func NewMemoryStore[K comparable, V any]() *MemoryStore[K, V] {
+	return &MemoryStore[K, V]{
+		data: make(map[K]V),
+	}
+}
+
+func (s *MemoryStore[K, V]) Set(ctx context.Context, key K, value V) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.data[key] = value
+	return nil
+}
+
+func (s *MemoryStore[K, V]) Get(ctx context.Context, key K) (V, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	val, ok := s.data[key]
+	if !ok {
+		var empty V
+		return empty, ErrNotFound
+	}
+	return val, nil
+}
+
+func (s *MemoryStore[K, V]) Delete(ctx context.Context, key K) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.data, key)
+	return nil
+}
