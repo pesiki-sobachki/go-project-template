57cd0668d3470804e2f43cb617b2619906964bde
Denis Beresnev <shanthi.bunch@gmail.com>
2025-12-03 22:32:54 +0300

    refactor

--- internal/adapter/inbound/http/middleware/security.go
+++ internal/adapter/inbound/http/middleware/security.go
@@ -1,15 +1,17 @@
 package middleware
 
 import (
+	"errors"
 	"net/http"
+
+	"github.com/shanth1/template/internal/pkg/response"
 )
 
 func RequestSizeLimit(bytes int64) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if r.ContentLength > bytes {
-				w.WriteHeader(http.StatusRequestEntityTooLarge)
-				_, _ = w.Write([]byte(`{"error": "request body too large"}`))
+				response.WithError(w, http.StatusRequestEntityTooLarge, errors.New("request body too large"))
 				return
 			}
 
